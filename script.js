08ed8f05329608d3ca86656945cc63729aaeba96
Oussamabouriga <123689537+Oussamabouriga@users.noreply.github.com>
2024-04-28 10:10:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!navbar.contains(event.target) && !menuIcon.contains(event.target)) {
+            navbar.classList.remove('active');
+        }
+    });
+});
